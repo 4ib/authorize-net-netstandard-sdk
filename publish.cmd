95ccdd8bd2cca38bb6a38fce60443dbad04d7256
Ruslan Keba <ruslan@4ib.com>
2023-04-21 18:29:47 +0300

    project renamed for compatibility with legacy framework package version

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,3 @@
 dotnet build AuthorizeNET\AuthorizeNet.Core.sln -c Release
-dotnet pack AuthorizeNET\AuthorizeNET\AuthorizeNET.csproj -c Release
-dotnet nuget push "AuthorizeNET\AuthorizeNET\bin\Release\AuthorizeNet.DotNetCore.*.nupkg" --source "github"
+dotnet pack AuthorizeNET\AuthorizeNET\AuthorizeNETStandard.csproj -c Release
+dotnet nuget push "AuthorizeNET\AuthorizeNET\bin\Release\AuthorizeNet.Standard.*.nupkg" --source "github"
